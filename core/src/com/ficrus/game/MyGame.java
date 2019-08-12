18e26a371877ea4b4c2761f63953fd0923ce4c07
artyomnurgaliev <artyomnurgaliev@mail.ru>
2019-08-12 16:05:38 +0300

    initial commit

---
+++ core/src/com/ficrus/game/MyGame.java
@@ -0,0 +1,182 @@
+package com.ficrus.game;
+
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.codeandweb.physicseditor.PhysicsShapeCache;
+
+import java.util.HashMap;
+import java.util.Random;
+
+public class MyGame extends ApplicationAdapter {
+    private static final float STEP_TIME = 1f / 60f;
+    private static final int VELOCITY_ITERATIONS = 6;
+    private static final int POSITION_ITERATIONS = 2;
+    private static final float SCALE = 0.05f;
+    private static final int COUNT = 10;
+
+    private TextureAtlas textureAtlas;
+    private SpriteBatch batch;
+    private final HashMap<String, Sprite> sprites = new HashMap<>();
+
+    private OrthographicCamera camera;
+    private ExtendViewport viewport;
+
+    private World world;
+    private Box2DDebugRenderer debugRenderer;
+    private PhysicsShapeCache physicsBodies;
+    private float accumulator = 0;
+    private Body ground;
+    private Body[] fruitBodies = new Body[COUNT];
+    private String[] names = new String[COUNT];
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        viewport = new ExtendViewport(50, 50, camera);
+
+        textureAtlas = new TextureAtlas("sprites.txt");
+        addSprites();
+
+        Box2D.init();
+        world = new World(new Vector2(0, -120), true);
+        physicsBodies = new PhysicsShapeCache("sprites.xml");
+        generateFruit();
+
+        debugRenderer = new Box2DDebugRenderer();
+    }
+
+    private void addSprites() {
+        Array<AtlasRegion> regions = textureAtlas.getRegions();
+
+        for (AtlasRegion region : regions) {
+            Sprite sprite = textureAtlas.createSprite(region.name);
+
+            float width = sprite.getWidth() * SCALE;
+            float height = sprite.getHeight() * SCALE;
+
+            sprite.setSize(width, height);
+            sprite.setOrigin(0, 0);
+
+            sprites.put(region.name, sprite);
+        }
+    }
+
+    private void generateFruit() {
+        String[] fruitNames = new String[]{"banana", "cherries", "orange"};
+
+        Random random = new Random();
+
+        for (int i = 0; i < fruitBodies.length; i++) {
+            String name = fruitNames[random.nextInt(fruitNames.length)];
+
+            float x = random.nextFloat() * 50;
+            float y = random.nextFloat() * 50 + 50;
+
+            names[i] = name;
+            fruitBodies[i] = createBody(name, x, y);
+        }
+    }
+
+    private Body createBody(String name, float x, float y){
+        Body body = physicsBodies.createBody(name, world, SCALE, SCALE);
+        body.setTransform(x, y, (float) 0);
+
+        return body;
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+
+        batch.setProjectionMatrix(camera.combined);
+
+        createGround();
+    }
+
+    private void createGround() {
+        if (ground != null) world.destroyBody(ground);
+
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.friction = 1;
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(camera.viewportWidth, 1);
+        fixtureDef.shape = shape;
+
+        ground = world.createBody(bodyDef);
+        ground.createFixture(fixtureDef);
+        ground.setTransform(0, 0, 0);
+
+        shape.dispose();
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0.57f, 0.77f, 0.85f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stepWorld();
+
+        batch.begin();
+
+        for (int i = 0; i < fruitBodies.length; i++) {
+            Body body = fruitBodies[i];
+            String name = names[i];
+
+            Vector2 position = body.getPosition();
+            float degrees = (float) Math.toDegrees(body.getAngle());
+            drawSprite(name, position.x, position.y, degrees);
+        }
+
+        batch.end();
+
+        // uncomment to show the polygons
+        // debugRenderer.render(world, camera.combined);
+    }
+
+    private void stepWorld() {
+        float delta = Gdx.graphics.getDeltaTime();
+        accumulator += Math.min(delta, 0.25f);
+
+        if (accumulator >= STEP_TIME) {
+            accumulator -= STEP_TIME;
+            world.step(STEP_TIME, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+        }
+    }
+
+    private void drawSprite(String name, float x, float y, float degrees) {
+        Sprite sprite = sprites.get(name);
+        sprite.setPosition(x, y);
+        sprite.setRotation(degrees);
+        sprite.draw(batch);
+    }
+
+    @Override
+    public void dispose() {
+        textureAtlas.dispose();
+        sprites.clear();
+        world.dispose();
+        debugRenderer.dispose();
+    }
+}
